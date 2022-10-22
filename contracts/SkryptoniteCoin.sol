9f6e6bdb6b9a880221deb1edc0e0fbff77d2c8cd
Artem Rozhenkov <ae.rozhenkov@tensor.ru>
2022-10-22 16:19:11 +0300

    submitted hw-3.5

---
+++ contracts/SkryptoniteCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SkryptoniteCoin is ERC20 {
+    address public ownerERC;
+
+    constructor() ERC20("Skryptonite Coin", "SKR") {
+        ownerERC = msg.sender;
+
+        _mint(ownerERC, 1000);
+    }
+}
